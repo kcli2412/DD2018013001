3f2b426155c18f6b8f1a108f3c6372f9ed888aba
kcli2412 <kcli2412@gmail.com>
2018-01-30 11:35:17 +0800

    ActivityCompat.requestPermission location.distanceTo

--- app/src/main/java/com/example/student/dd2018013001/MainActivity.java
+++ app/src/main/java/com/example/student/dd2018013001/MainActivity.java
@@ -5,11 +5,15 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
     LocationManager lm;
@@ -23,6 +27,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void click1(View v) {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION}, 321);
+            return;
+        }
+        else
+        {
+            startLoc();
+        }
+    }
+
+    private void startLoc() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -35,6 +58,22 @@ public class MainActivity extends AppCompatActivity {
         }
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new MyListener());
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode)
+        {
+            case 321:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    startLoc();
+                }
+                break;
+        }
+    }
 }
 
 class MyListener implements LocationListener
@@ -42,7 +81,14 @@ class MyListener implements LocationListener
 
     @Override
     public void onLocationChanged(Location location) {
-        Log.d("LOC", "Changed!!");
+        Log.d("LOC", "Changed!! " + location.getLatitude() + ", " + location.getLongitude());
+
+        Location loc101 = new Location("LOC");
+        loc101.setLatitude(25.0336);
+        loc101.setLongitude(121.5646);
+
+        float dist = location.distanceTo(loc101);
+        Log.d("LOC", "Dist: " + dist);
     }
 
     @Override
